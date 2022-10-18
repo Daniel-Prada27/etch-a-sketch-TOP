98706fbe20e9839a28370ffa06cc1b9e7e7f1604
Daniel <danielspradah0@gmail.com>
2022-10-18 12:04:00 -0500

    Add color change on click

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,14 @@
 const sizeBtn = document.getElementById('pad-size-button');
 const sizeTell = document.getElementById('pad-size-teller');
 
+
 function createGrid(number){
     const mainContainer = document.getElementById('main-container');
     mainContainer.style.cssText = `grid-template-columns: repeat(${number}, 1fr); grid-template-rows: repeat(${number}, 1fr);`
     for(let i = 0; i < number*number; i++){
         let divFromGrid = document.createElement('div');
         divFromGrid.classList.add("cell")
+        divFromGrid.setAttribute('id', `number-${i}`)
         mainContainer.appendChild(divFromGrid);
     }
     sizeTell.textContent = `The pad is ${number}x${number} squares`
@@ -16,7 +18,7 @@ createGrid(16);
 
 function getSize(){
     const mainContainer = document.getElementById('main-container');
-    let squares = prompt("Input: ");
+    let squares = prompt("Input the pad size: ");
     let floorSquares = Math.floor(squares);
     if(isNaN(squares) || squares == ""){
         alert("Must enter a number!");
@@ -35,5 +37,22 @@ function getSize(){
     
 }
 
-sizeBtn.addEventListener('click', getSize);
+let eachCell = document.querySelectorAll('.cell');
 
+for (let i = 0; i < eachCell.length; i++) {
+    eachCell[i].addEventListener('mouseover', getId);
+}
+// eachCell.addEventListener('click', getId);
+
+function getId(event){
+    if(event.buttons == 1 || event.buttons == 3){
+    console.log("1 or 3")
+    }
+    event.srcElement.style.cssText = "background-color: black;";
+    console.log(event.srcElement.id);
+}
+
+function coloring(id){
+    let individualCell = document.getElementById(`number-${id}`);
+    individualCell.style.cssText = "background-color: black;";
+}
